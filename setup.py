71dfdfe48ca99056b3141a371b68079c0b989983
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-06-14 20:21:42 +1000

    Bundle the tar balls for Apache and other required packages so updates and moving of old files don't cause failures.

--- setup.py
+++ setup.py
@@ -146,7 +146,7 @@ for root, dirs, files in os.walk('src/httpd', topdown=False):
         print('adding ', path)
 
 setup(name = 'mod_wsgi-httpd',
-    version = '%s.5' % HTTPD_VERSION,
+    version = '%s.6' % HTTPD_VERSION,
     description = 'Installer for Apache httpd server.',
     author = 'Graham Dumpleton',
     author_email = 'Graham.Dumpleton@gmail.com',
